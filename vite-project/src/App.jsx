39773518ccf0d970171f9a9f1c542ef8cacfc733
shoro <shorowitz03@gmail.com>
2022-02-23 20:47:08 -0500

    new thing

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import logo from './favicon.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Samuel Horowitz</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/sam-horowitz/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://github.com/shorowitz03"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
