61761e47f036f074abe75f8e6bcf03cbff32b655
shoro <shorowitz03@gmail.com>
2022-02-23 21:26:33 -0500

    what

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Samuel Horowitz</p>
+        <p>bernardo de barros barreto costa machado</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
